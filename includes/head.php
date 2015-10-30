ed9f1c0a13cbb2a72c04fba4dbff09b9453a9ea5
Matt Blair <mdrblair@gmail.com>
2015-10-30 18:28:17 +0000

    Adding revised map and associated styles along with meta info for responsive testing

--- includes/head.php
+++ includes/head.php
@@ -2,6 +2,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 
     <title>Home - The National Archives</title>
 
