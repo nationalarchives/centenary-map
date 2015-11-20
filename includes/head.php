e2a6132d2e8bf173b9ffef254389ceca9ff8c687
Matt Blair <mdrblair@gmail.com>
2015-11-20 16:34:33 +0000

    Formatting changes

--- includes/head.php
+++ includes/head.php
@@ -32,7 +32,6 @@
     <script type="text/javascript" src="http://livelb.nationalarchives.gov.uk/wp-content/themes/tna/scripts/respond.min.js"></script>
     <![endif]-->
 
-
     <!-- iOS viewport fix -->
     <script src="http://www.nationalarchives.gov.uk/wp-content/themes/tna/scripts/ios-viewport-fix.js"></script>
 
